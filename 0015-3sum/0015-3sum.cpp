ed637ada37d116a201d57107317beaf7812e6f3b
Ayushman Rai <raiayush72757@gmail.com>
2026-09-08 22:52:37 +0530

    Time: 51 ms (41.33%), Space: 35.9 MB (10.67%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,42 +1,57 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        int n = nums.size();
-        
-        sort(nums.begin(), nums.end());
-        
-        for(int i = 0; i < n; i++) {
-            // Skip duplicate elements for i
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
-            
-            int left = i + 1;
-            int right = n - 1;
-            
-            while(left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                
-                if(sum == 0) {
-                    ans.push_back({nums[i], nums[left], nums[right]});
-                    
-                    // Skip duplicates for left
-                    while(left < right && nums[left] == nums[left + 1]) left++;
-                    
-                    // Skip duplicates for right
-                    while(left < right && nums[right] == nums[right - 1]) right--;
-                    
-                    left++;
-                    right--;
+    vector<vector<int>> twoSum(vector<int>& nums, int l, int r, int target) {
+        vector<vector<int>> duplets{};
+
+        while (l < r) {
+            int sum{nums[l] + nums[r]};
+
+            if (sum < target) {
+                l++;
+            } else if (sum > target) {
+                r--;
+            } else {
+                duplets.push_back({l, r});
+
+                int leftValue{nums[l]};
+                int rightValue{nums[r]};
+
+                while (l < r && nums[l] == leftValue) {
+                    l++;
                 }
-                else if(sum < 0) {
-                    left++;
-                }
-                else {
-                    right--;
+
+                while (l < r && nums[r] == rightValue) {
+                    r--;
                 }
             }
         }
-        
-        return ans;
+
+        return duplets;
+    }
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int size{static_cast<int>(nums.size())};
+        vector<vector<int>> result{};
+
+        for (int i{0}; i < size - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int required_num{-nums[i]};
+
+            int l{i + 1};
+            int r{size - 1};
+
+            vector<vector<int>> duplets{twoSum(nums, l, r, required_num)};
+
+            for (const auto& duplet : duplets) {
+                result.push_back({nums[i], nums[duplet[0]], nums[duplet[1]]});
+            }
+        }
+
+        return result;
     }
 };
